6d2f0746e1b1c6d7a185a0c64b8b954a37cd0fa8
warblo001 <67009988+warblo001@users.noreply.github.com>
2021-03-23 22:02:32 +0100

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    flash.notice = "User #{@user.name} successfully created!"
+    redirect_to user_path(@user)
+  end
+
+  def index
+    @users = User.all
+  end
+end
